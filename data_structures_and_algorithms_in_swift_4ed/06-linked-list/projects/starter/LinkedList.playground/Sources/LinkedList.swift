90c4761c21712d5b30851463aba9455d34d29522
Plamen Andreev <plam4u@me.com>
2022-05-03 20:07:17 +0200

    Following book exercises on LinkedList

---
+++ data_structures_and_algorithms_in_swift_4ed/06-linked-list/projects/starter/LinkedList.playground/Sources/LinkedList.swift
@@ -0,0 +1,141 @@
+import Darwin
+public struct LinkedList<Value> {
+
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+
+    public init() {}
+
+    public var isEmpty: Bool {
+        head == nil
+    }
+
+    public mutating func push(_ value: Value) {
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+
+    @discardableResult
+    public mutating func pop() -> Value? {
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+
+    public mutating func append(_ value: Value) {
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        tail!.next = Node(value: value)
+        tail = tail!.next
+    }
+
+    @discardableResult
+    public mutating func removeLast() -> Value? {
+        guard let head = head else {
+            return nil
+        }
+        guard head.next != nil else {
+            return pop()
+        }
+        var prev = head
+        var current = head
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        prev.next = nil
+        tail = prev
+        return current.value
+    }
+
+    public func node(at index: Int) -> Node<Value>? {
+        var currentNode = head
+        var currentIndex = 0
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode!.next
+            currentIndex += 1
+        }
+        return currentNode
+    }
+
+    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+        guard tail !== node else {
+            append(value)
+            return tail!
+        }
+        node.next = Node(value: value, next: node.next)
+        return node.next!
+    }
+
+    @discardableResult
+    public mutating func remove(after node: Node<Value>) -> Value? {
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
+}
+
+extension LinkedList: Collection {
+
+    public struct Index: Comparable {
+
+        public var node: Node<Value>?
+
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node) { $0?.next }
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+
+    public var startIndex: Index {
+        Index(node: head)
+    }
+
+    public var endIndex: Index {
+        Index(node: tail?.next)
+    }
+
+    public func index(after i: Index) -> Index {
+        Index(node: i.node?.next)
+    }
+
+    public subscript(position: Index) -> Value {
+        position.node!.value
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty list"
+        }
+        return String(describing: head)
+    }
+}
